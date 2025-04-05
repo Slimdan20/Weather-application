cac4532f10ca42f78d99234f5c55a7aa33e7dca0
Daniel Efe <daneytechwriter@gmail.com>
2025-04-04 17:18:02 -0700

    new commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            globals: globals.browser,
+        },
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            eqeqeq: 'error',
+            'no-var': 'error',
+            'prefer-const': 'warn',
+            'no-global-assign': 'error',
+            'no-param-reassign': 'warn',
+            complexity: ['warn', { max: 16 }],
+            curly: 'error',
+            'no-debugger': 'error',
+        },
+    },
+])
